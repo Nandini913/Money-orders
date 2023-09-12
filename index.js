f44d545287c99adaff4ccf77b58372d58d8b8d7d
tejasnoovosoft <tejas.ekhande@noovosoft.com>
2023-09-12 11:42:21 +0530

    Add Drop-Down List of Users in Dashboard

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const login  = require('./APIS/login');
 const deposit = require('./APIS/deposit');
 const withdraw = require('./APIS/withdraw');
 const transfer = require('./APIS/transfer');
+const users = require('./APIS/fetchUsers');
 app.use(express.static('layouts'));
 app.use(express.json());
 app.use('/register' , register);
@@ -13,6 +14,11 @@ app.use('/login',login);
 app.use('/deposit',deposit);
 app.use('/withdraw',withdraw);
 app.use('/transfer',transfer);
+app.use('/users',users);
+app.get('/test',(req, res) => {
+    console.log("111");
+    res.send("aa");
+});
 app.listen(port , (req,res) => {
     console.log("Server was running on port 3000");
 });
